11ee6929f5f91b259d1105814aec07e86eff3d26
Soogyurng Kwan <soogyurngx.kwan@intel.com>
2018-06-27 05:39:24 -0700

    Script update for print button and accordion menu

--- proto/js/script.js
+++ proto/js/script.js
@@ -1,8 +1,29 @@
+/**************************/
+/* Author: Soo            */
+/* Last update: 6/27/2018 */
+/**************************/
+
+var accordion = null;
+var viewport = $(window).width();
+
+//if viewport of the browser is less than 759. Accordion menu is disabled
+if ( viewport <= 759) {
+    accordion = true;
+    //Print button is hidden 
+    $( print_button ).hide();
+} else {
+    accordion = false;
+}
+
+if (accordion == false) {
     $(function() {
-        $( "#accordion" ).accordion({
-            header: "h2",
-            active: 0,
-            collapsible: false,
-            heightStyle: "content"
-        });
+        $( "#accordion" ).accordion( "disable" );
     });
+} else {
+    $( "#accordion" ).accordion({
+        header: "h2",
+        active: 0,
+        collapsible: false,
+        heightStyle: "content",
+    });
+};
