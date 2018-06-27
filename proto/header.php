ab7e4225bc3941af2881cd9e17d25e2b26a36b96
ybeausoleil <ybeausol@gmail.com>
2018-06-26 20:46:38 -0700

    php includes test

---
+++ proto/header.php
@@ -0,0 +1,13 @@
+<header class="container-fluid">
+	<div class="row">
+		<div class="col-sm-2">
+			<a href="https://www.bcit.ca/"><img  src="images/logo.png" alt="BCIT logo" /></a><br />
+			<a class="header-site" href="https://my.bcit.ca/">myBCIT</a>
+			<a class="header-site" href="https://learn.bcit.ca/">D2L</a>
+		</div>
+		<div class="col-lg-auto">
+			<h1>COMP1950 - Web Development And Design 2</h1>
+			<h3>Thursday Evening Class</h3>
+		</div>
+	</div>
+</header>
---
+++ proto/nav-quiz.php
@@ -0,0 +1,22 @@
+<nav class="container-fluid navbar navbar-expand-lg navbar-light bg-light">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+	</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="nav nav-pills navbar-nav mr-auto">
+    <li class="nav-item"><a class="nav-link" href="homepage.html">Home</a></li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Previous Quizzes and Grades</a>
+        <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Quiz 1</a>
+        <a class="dropdown-item disabled" href="#">Quiz 2</a>
+        <a class="dropdown-item disabled" href="#">Quiz 3</a>
+        <a class="dropdown-item disabled" href="#">Quiz 4</a>
+        <a class="dropdown-item disabled" href="#">Quiz 5</a>
+        <a class="dropdown-item disabled" href="#">Quiz 6</a>
+        <a class="dropdown-item disabled" href="#">Quiz 7</a>
+        <a class="dropdown-item disabled" href="#">Quiz 8</a>
+        </div>
+    </li>
+  </div>
+</nav>
